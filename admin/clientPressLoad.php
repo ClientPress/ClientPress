10c11002998671d5d64f91541f8c8712704a2a35
Skemp45 <Shawn@besmartdesigns.com>
2016-10-05 20:52:10 -0700

    temp added bootstrap files to admin

--- admin/clientPressLoad.php
+++ admin/clientPressLoad.php
@@ -47,6 +47,11 @@ add_action('admin_menu', 'ClientPress_menu');
 
           //Build Page
           ?>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+          <script type="text/javascript" src="admin/js/app.js"></script>
+          <link rel="stylesheet" href="admin/css/main.css">
           <div class="container-fluid">
           <h1>ManageWP Project</h1>
           <div class="container-fluid">
